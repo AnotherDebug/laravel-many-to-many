c3ee9fb22b57c1446ce22f7ab658e69d84b00375
AnotherDebug <antonio.faddanno@gmail.com>
2023-12-04 19:09:37 +0100

    [ADD]: card body in admin home blade

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('admin.home');
+        $projects = Project::count();
+        $technologies = Technology::count();
+        $types = Type::count();
+        return view('admin.home', compact('projects', 'technologies', 'types'));
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,11 +1,29 @@
 @extends('layouts.admin')
 
 @section('content')
+    <div class="adminHome container">
+        <h1 class="m-3 text-center">Home Dashboard</h1>
 
-<div class="adminHome">
-    <h1 class="m-3 text-center">Home Dashboard</h1>
-</div>
+        <div class="card">
+            <h5 class="card-header">Featured</h5>
+            <div class="card-body">
+                <h5 class="card-title">Projects</h5>
+                <p class="card-text">Currently there are n.<strong>{{ $projects }}</strong>.</p>
 
+            </div>
+            <div class="card-body bg-body-secondary ">
+                <h5 class="card-title">Technologies</h5>
+                <p class="card-text">Currently there are n.<strong>{{ $technologies }}</strong>.</p>
+
+            </div>
+            <div class="card-body bg-body-tertiary ">
+                <h5 class="card-title">Types</h5>
+                <p class="card-text">Currently there are n.<strong>{{ $types }}</strong>.</p>
+
+            </div>
+        </div>
+
+    </div>
 @endsection
 
 @section('title')
